378bc6f26cf5a5bb632ad8b93d159c690a2d064a
Mike <mlaitw@gmail.com>
2023-08-15 20:52:11 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+
+
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`express is running on http://localhost:${port}`)
+})
+
