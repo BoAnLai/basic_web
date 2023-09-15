210f84dbbc283df8465aad77ef92d9eff841b2ff
Mike <mlaitw@gmail.com>
2023-09-16 01:24:12 +0800

    dynamic router

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-const exphbs = require('express-handlebars')
-
+const { engine } = require('express-handlebars')
 
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.set('views', './views')
+//res.render go the path "/views/"
+
+app.use(express.static('public'))
+//static resource go the path "/public/" so if client type "/javascript/a.js", he'll get the file /public/javascript/a.js
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('homepage')
+})
+
+app.get('/:dynamicRouter', (req, res) => {
+  res.render(req.params.dynamicRouter)
 })
 
 
---
+++ updateDirection.md
@@ -0,0 +1,30 @@
+# update direction
+
+0.app.js 完成動態路由 >>> done
+
+1.main.handlebars
+  下拉式選單修正 >>> done
+  navbar新增留言板 >>> done
+
+2.homepage.handlebars
+  google map api
+
+3.resume.handlebars
+  嵌入pdf(preview) >>> done
+  RWD調整pdf
+
+4.產生QR code
+
+5.cache
+  如果有人重複進入
+
+6.留言板message board
+  雛形
+  匯入SQL
+  紀錄ip
+
+5.side-project frame the layouts
+6.side-project
+7.log-in and password security
+8.金流
+[EOF]
